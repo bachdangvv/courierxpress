914c46a3776e65edc5f6c26e9cbb94cb8873837e
Ketaming <mingduu05@gmail.com>
2025-10-19 22:55:26 +0700

    abcxyz

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>CourierXpress</title>
+        </head>
+        <body>
+            <h1>Xin chào Hội đồng quản trị chatGPT</h1>
+        </body>
+    </html>
