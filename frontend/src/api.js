627459817b94167678fec07b0d90db4978e54741
ustrungnguyen <contact@ngtrung.com>
2025-10-21 00:12:45 +0700

    Removed gitignore

--- frontend/src/api.js
+++
@@ -1,6 +0,0 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
-
-export async function getCourierLocation(courierId) {
-  const res = await fetch(`${API_URL}/couriers/${courierId}/location`);
-  return await res.json();
-}
