40d88707b0d8c1bf7945b111a03ebbfe984366dd
Bach Dang <bachdangvv@gmail.com>
2025-10-20 21:00:31 +0700

    cai lai laravel va react

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-            <title>CourierXpress</title>
-        </head>
-        <body>
-            <h1>Xin chào Hội đồng quản trị chatGPT</h1>
-        </body>
-    </html>
