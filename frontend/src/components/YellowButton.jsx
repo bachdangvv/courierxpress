d186af02903065e46c7c95c665ee0afa8e8531fc
ustrungnguyen <contact@ngtrung.com>
2025-10-25 22:04:35 +0700

    About us

--- frontend/src/components/YellowButton.jsx
+++ frontend/src/components/YellowButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const YellowButton = ({children}) => {
     return (
         <button
-            className="order-btn w-full md:w-[145px] h-[45px] rounded-[25px] opacity-100 bg-amber-400"
+            className="order-btn w-full md:w-[145px] h-[45px] rounded-[25px] opacity-100 bg-amber-400 p-2 px-4"
         >
         {children}</button>
 );
