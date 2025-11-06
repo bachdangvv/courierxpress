0c192f3a4006fa97be029b33c4c32188ca3beb86
Ketaming <mingduu05@gmail.com>
2025-11-06 19:44:59 +0700

    abc

--- frontend/src/components/agentspage/AgentDashboard.jsx
+++ frontend/src/components/agentspage/AgentDashboard.jsx
@@ -5,7 +5,7 @@ import AgentHeader from "./AgentHeader";
 
 export default function AgentDashboard() {
   return (
-    <div className="flex h-screen bg-gray-50 font-sans">
+    <div className="flex h-screen bg-gray-50 font-sans mt-[70px]">
       {/* Sidebar */}
       <AgentSidebar />
 
